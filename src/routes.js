a8a7078d41de5934e5d8cc13fbf9df7ac05c17a2
theory friction practice <jackson.brebnor@gmail.com>
2017-03-22 14:07:41 -0400

    new materialui

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,37 @@
 import React, { Component, } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Text from 'material-ui/Text';
+
+// import * from 'material-ui/styles/theme';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { BrowserRouter, Link, Route, Switch, } from 'react-router-dom';
-import { Boiler, NoMatch } from './components';
+import { Boiler, NoMatch, } from './components';
 
 export class Routes extends Component {
   
   render () {
     return (
-  <BrowserRouter>
-    <div className="Game">
-      <h1>Rummy Redux</h1>
-      <div className="container">
-        <Switch>
-          <Route path="/"  component={Boiler} />
-          <Route component={NoMatch}/>
-        </Switch>
-      </div>
-    </div>
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <div className="Game">
+          <AppBar >
+            <Toolbar>
+           <Text type="title" colorInherit>Title</Text>
+            </Toolbar>
+       </AppBar>
+          <h1>Rummy Redux</h1>
+          <div className="container">
+            <Switch>
+              <Route path="/" component={Boiler} />
+              <Route component={NoMatch}/>
+            </Switch>
+          </div>
+        </div>
   </BrowserRouter>
-
+</MuiThemeProvider>
     );
   }
 }
