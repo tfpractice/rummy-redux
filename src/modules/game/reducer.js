3804bea127f4db6f8a71b145838b11cb26998fd0
theory friction practice <jackson.brebnor@gmail.com>
2017-03-22 15:12:57 -0400

    basic reducer

--- src/modules/game/reducer.js
+++ src/modules/game/reducer.js
@@ -1,6 +1,7 @@
-import { GAME_ACTIONS, } from './constants';
 import { Game, } from 'rummy-rules';
 import { Deck, } from 'bee52';
+import { GAME_ACTIONS, } from './constants';
+
 const { shuffle, deck, } = Deck;
 const init = Game.setDeck(shuffle(deck()))(Game.game());
 
