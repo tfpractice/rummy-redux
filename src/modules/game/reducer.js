30669b2c5bb83a61356e40a4b821c8c1b8487060
theory friction practice <jackson.brebnor@gmail.com>
2017-03-22 17:17:08 -0400

    init players

--- src/modules/game/reducer.js
+++ src/modules/game/reducer.js
@@ -1,9 +1,10 @@
-import { Game, } from 'rummy-rules';
+import { Game, Player, } from 'rummy-rules';
 import { Deck, } from 'bee52';
 import { GAME_ACTIONS, } from './constants';
-
+const { player, } = Player;
 const { shuffle, deck, } = Deck;
-const init = Game.setDeck(shuffle(deck()))(Game.game());
+const iPlrs = [ player('p0'), player('p1'), ];
+const init = Game.setDeck(shuffle(deck()))(Game.game(iPlrs));
 
 export default (state = init, { type, curry, }) =>
   GAME_ACTIONS.has(type) ? curry(state) : state;
