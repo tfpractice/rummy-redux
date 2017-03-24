9c4e44814af3082cbdda0e16e0433dc27801c66b
theory friction practice <tfpractice@theorys-MacBook-Air.local>
2017-03-23 21:23:58 -0400

    chips

--- src/components/deck.js
+++ src/components/deck.js
@@ -5,12 +5,19 @@ import customPropTypes from 'material-ui/utils/customPropTypes';
 import Paper from 'material-ui/Paper';
 import { Tab, Tabs, } from 'material-ui/Tabs';
 import PlayCard from './playCard';
+import CardChip from './cards/chip';
 
 const mapStateToProps = ({ game: { deck, }, }) => ({ deck, });
 
 const Deck = ({ deck, }) => (
   <div>
-    {deck.map((c, i) => <PlayCard card={c} pos={i} key={c.id}/>)}
+    <div>
+      {deck.map((c, i) => <PlayCard card={c} pos={i} key={c.id}/>)}
+    </div>
+
+    <div>
+      {deck.map((c, i) => <PlayCard card={c} pos={i} key={c.id}/>)}
+    </div>
   </div>);
 
 export default connect(mapStateToProps)(Deck);
