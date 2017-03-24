24057162ac75bfcb18fee410494269bf81a9a2e7
theory friction practice <jackson.brebnor@gmail.com>
2017-03-24 14:09:17 -0400

    its a firebase issue

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import Button from 'material-ui/Button';
 
-// import TextField from 'material-ui/TextField';
+import TextField from 'material-ui/TextField';
+
 //
 // import * as MRF from 'redux-form-material-ui';
 // console.log('MRF', MRF);
 
-import { TextField, } from 'redux-form-material-ui';
+// import { TextField, } from 'redux-form-material-ui';
 import { connect, } from 'react-redux';
 import { Field, reduxForm, } from 'redux-form';
 import { resetForm, } from '../../utils';
 import { AuthActs, } from '../../modules';
+
+// console.log('reduxForm', reduxForm);
 
 const renderTextField = ({ input, meta: { touched, error, }, ...custom }) => (
     <TextField id="displayName" type="text" inputProps={input} error={error} {...custom}/>);
 
 const baseLogin = ({ handleSubmit, }) => (
   <form onSubmit={handleSubmit} >
-    <Field name="displayName" component={TextField}/>
+    <Field name="displayName" type="text" component={renderTextField}/>
     <Button accent type="submit">Login</Button>
   </form>
 );
@@ -25,11 +28,12 @@ const ReduxLogin = reduxForm()(baseLogin);
 
 const LoginForm = ({ login, formID, }) => (
   <div className="row">
-    <Button onClick={ () => login({ displayName: 'nonform', })}/>
+    <Button onClick={ () => login({ displayName: 'nonform', })}>log</Button>
     <ReduxLogin
       form={formID} onSubmit={login} onSubmitSuccess={resetForm(formID)}
     />
   </div>
 );
 
+console.log('AuthActs', AuthActs);
 export default connect(null, AuthActs)(LoginForm);
