6de8c3fa6ecf1479f1da4a7bdeee7a9d83954508
theory friction practice <jackson.brebnor@gmail.com>
2017-03-25 19:30:37 -0400

    logoutlink

--- src/components/auth/logoutLink.js
+++ src/components/auth/logoutLink.js
@@ -1,9 +1,9 @@
 import React, { PropTypes, } from 'react';
 import Button from 'material-ui/Button';
 import { connect, } from 'react-redux';
-
 import { AuthActs, } from '../../modules';
 
-const LogoutLink = ({ logout, }) => <Button onClick={logout}>Logout</Button>;
+const LogoutLink = ({ logout, }) =>
+ <Button contrast onClick={logout}>Logout</Button>;
 
 export default connect(null, AuthActs)(LogoutLink);
