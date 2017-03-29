d7eaa1e4c2b66023f62835cd68660a1827df849d
theory friction practice <jackson.brebnor@gmail.com>
2017-03-29 16:17:20 -0400

    swipable

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,6 +5,18 @@ import logo from './logo.svg';
 import './main.css';
 import Game from '../game';
 
+import SwipeableViews from 'react-swipeable-views';
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: { background: '#FEA900', },
+  slide2: { background: '#B3DC4A', },
+  slide3: { background: '#6AC0FF', },
+};
+
 class Main extends Component {
   render() {
     return (
@@ -13,8 +25,22 @@ class Main extends Component {
           <Text type="headline">Welcome to Rummy Redux</Text>
           <Text type="headline">Sign in to start a new game</Text>
         </Layout>
-        <Layout xs={12} />
-        <Game/>
+        <Layout item>
+          <SwipeableViews enableMouseEvents>
+            <div style={Object.assign({}, styles.slide, styles.slide1)}>
+              slide n°1
+            </div>
+            <div style={Object.assign({}, styles.slide, styles.slide2)}>
+              slide n°2
+            </div>
+            <div style={Object.assign({}, styles.slide, styles.slide3)}>
+              slide n°3
+            </div>
+          </SwipeableViews>
+        </Layout>
+        <Layout xs={12} >
+          <Game/>
+        </Layout>
       </Layout>
     );
   }
