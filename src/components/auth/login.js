c59fe6632f88dbd4903090ca09d4b10b2e2d6429
theory friction practice <jackson.brebnor@gmail.com>
2017-03-29 14:33:42 -0400

    login display

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { connect, } from 'react-redux';
 import { Field, reduxForm, } from 'redux-form';
+import Layout from 'material-ui/Layout';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import LogoutLink from './logoutLink';
 import { resetForm, } from '../../utils';
 import { AuthActs, } from '../../modules';
 
-const renderTextField = ({ input, meta: { touched, error, }, ...custom }) => (
-    <TextField id="displayName" type="text" inputProps={input} error={error} {...custom}/>);
+const styles = { display: 'inline-flex', };
+
+const renderField = ({ input, meta: { error: e, }, ...rest }) => (
+    <TextField type="text" inputProps={input} error={e} {...rest}/>
+  );
 
 const baseLogin = ({ handleSubmit, }) => (
-  <form onSubmit={handleSubmit} >
-    <Field name="displayName" type="text" component={renderTextField}/>
+  <form onSubmit={handleSubmit} style={styles} >
+    <Field name="displayName" component={renderField}/>
     <Button accent type="submit">Login</Button>
     <LogoutLink/>
   </form>
 );
+
 const ReduxLogin = reduxForm()(baseLogin);
 
 const LoginForm = ({ login, formID, }) => (
-    <ReduxLogin
-      form={formID} onSubmit={login} onSubmitSuccess={resetForm(formID)}
-    />
+  <ReduxLogin
+    form={formID} onSubmit={login} onSubmitSuccess={resetForm(formID)}
+  />
 );
 
 export default connect(null, AuthActs)(LoginForm);
