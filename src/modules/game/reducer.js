ad397d588cdf914ebc21047bda1981c2521ee33f
theory friction practice <jackson.brebnor@gmail.com>
2017-04-01 22:09:04 -0400

    normalizr is weird

--- src/modules/game/reducer.js
+++ src/modules/game/reducer.js
@@ -1,3 +1,4 @@
+import { denormalize, normalize, schema, } from 'normalizr';
 import { Game, Player, } from 'rummy-rules';
 import { Deck, } from 'bee52';
 import { db, } from '../../utils/firebase';
@@ -8,9 +9,27 @@ const { shuffle, deck, } = Deck;
 const iPlrs = [ player('computer'), ];
 const init = Game.game(iPlrs, shuffle(deck()), []);
 
-export default (state = init, { type, curry, }) => {
-  console.log('JSON.stringify(state)', JSON.stringify(state));
+const cSchema = new schema.Object('card');
+const setSchema = new schema.Object('set', [ cSchema, ]);
+const pSchema = new schema.Entity('players', {
+  hand: [ cSchema, ],
+  sets: [ setSchema, ],
+});
 
-  db.ref('game').set(JSON.stringify(state));
+const dkSchema = new schema.Entity('deck', [ cSchema, ]);
+const dsSchema = new schema.Entity('discard', [ cSchema, ]);
+
+const gSchema = new schema.Entity('game', {
+  players: [ pSchema, ],
+  deck: [ dkSchema, ],
+  discard:  [ cSchema, ],
+}, { idAttribute: g => 'current', });
+
+console.log('normalize(state,gSchema)', normalize(init, gSchema));
+console.log('denormalize', denormalize(init, gSchema));
+
+export default (state = init, { type, curry, }) => {
+  const o = 0;
+  
   return GAME_ACTIONS.has(type) ? curry(state) : state;
 };
