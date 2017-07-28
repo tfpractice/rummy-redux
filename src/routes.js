97a38a4bf96e8e6f7e8d9b9883fc5c56624a00c0
theory friction practice <jackson.brebnor@gmail.com>
2017-07-28 10:24:41 -0400

    theme upgrades

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,39 @@
 import React, { Component, } from 'react';
 import { connect, } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import createPalette, { dark, } from 'material-ui/styles/palette';
-import createMuiTheme from 'material-ui/styles/theme';
-import pink from 'material-ui/colors/pink';
+
+import createPalette from 'material-ui/styles/palette';
+import { createMuiTheme, MuiThemeProvider, } from 'material-ui/styles/';
 import teal from 'material-ui/colors/teal';
+import pink from 'material-ui/colors/pink';
+import red from 'material-ui/colors/red';
+import grey from 'material-ui/colors/grey';
+
 import Grid from 'material-ui/Grid';
 import { BrowserRouter, Link, Route, Switch, } from 'react-router-dom';
 import { Game, Main, NavBar, NoMatch, } from './components';
 
+const palette = createPalette({ primary: red, accent: grey, type: 'dark', });
+
+export const theme = createMuiTheme({ palette, });
+
 const mapStateToProps = ({ users, }) => ({ users, });
 
-const palette = createPalette({
-  primary: teal,
-  accent: pink,
-  type: 'dark',
-  ...dark,
-});
-
-const { styleManager, theme, } = MuiThemeProvider.createDefaultContext(
-  { theme: createMuiTheme({ palette, }), });
-
-const styles = { paddingTop: '3rem', };
+const styles = { paddingTop: '5rem', };
 
 export class Routes extends Component {
-  render () {
+  render() {
     return (
-      <MuiThemeProvider theme={theme} styleManager={styleManager}>
+      <MuiThemeProvider theme={theme}>
         <BrowserRouter>
           <Grid container direction={'column'}>
             <Grid item xs={12}>
-              <NavBar/>
+              <NavBar />
             </Grid>
             <Grid item xs={12} style={styles}>
               <Switch>
                 <Route exact path="/" component={Main} />
                 <Route path="/play" component={Game} />
-                <Route component={NoMatch}/>
+                <Route component={NoMatch} />
               </Switch>
             </Grid>
           </Grid>
