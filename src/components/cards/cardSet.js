2b4d4e788be99aba44c4b27dd8223ed86c504e6f
theory friction practice <jackson.brebnor@gmail.com>
2017-08-15 17:56:22 -0400

    cardset

--- src/components/cards/cardSet.js
+++ src/components/cards/cardSet.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import CardIcon from './icons';
-import Grid from 'material-ui/Grid';
+import Grid from 'material-ui/Grid'; import CardIcon from './icons';
 
-const CardSet = ({ cards, }) => (
-  <Grid container justify={'center'} gutter={8} >
-    {cards.map(c => <CardIcon key={c.id} card={c}/>)}
-  </Grid>
-);
+const CardSet = ({ cards }) =>
+  (<Grid container justify="center" gutter={8}>
+    {cards.map(c => <CardIcon key={c.id} card={c} />)}
+  </Grid>);
 
 export default CardSet;
